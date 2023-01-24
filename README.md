a47c86a8762091bea195afe82c1876019b009bbb
flow6852 <flow6852math@gmail.com>
2023-01-24 17:06:25 +0900

    add README

---
+++ README.md
@@ -0,0 +1,23 @@
+# ddu-source-qf
+
+quick fix source for ddu.vim
+
+# Required
+
+## denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+## ddu.vim
+
+https://github.com/Shougo/ddu.vim
+
+## ddu-kind-file
+
+https://github.com/Shougo/ddu-kind-file
+
+# Configuration
+
+```
+call ddu#start(#{sources: [#{name: 'qf'}] })
+```
--- denops/@ddu-sources/qf.ts
+++ denops/@ddu-sources/qf.ts
@@ -1,19 +1,29 @@
-import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v2.2.0/types.ts";
+import { BaseSource, Item, sourceOptions } from "https://deno.land/x/ddu_vim@v2.2.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.2.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.2/file.ts";
+
+type Params = {
+   "loc": boolean; 
+}
 
 export class Source extends BaseSource<Params> {
   override kind = "file";
   override gather(args :{
       denops: Denops;
+      sourceOptions: SourceOptions;
+      sourceParams: Params;
   }): ReadableStream<Item<ActionData>[]>{
     return new ReadableStream<Item<ActionData>[]>({
       async start(controller) {
-        const clist = await fn.getqflist(args.denops) as Array<string>;
+        let clist = await fn.getqflist(args.denops) as Array<string>;
+        if (args.sourceParams.loc){
+            clist = await fn.getloclist(args.denops) as Array<string>;
+        }
         let items:  Item<ActionData>[] = [];
+        let regexp:RegExp = /(\s|\t|\n|\v)+/g;
         for(let citem of clist){
             items.push({
-                word: await fn.bufname(args.denops, citem.bufnr) + ":" + citem.text,
+                word: (await fn.bufname(args.denops, citem.bufnr) + ":" + citem.text).replaceAll(regexp, " "),
                 action: {
                     path: await fn.bufname(args.denops, citem.bufnr)
                 },
@@ -27,6 +37,7 @@ export class Source extends BaseSource<Params> {
 
   override params(): Params {
     return {
+        "loc" : false,
     };
   }
 }
