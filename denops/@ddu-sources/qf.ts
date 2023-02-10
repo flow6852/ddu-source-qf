88320522339f52d85d775bf2ab1735d0aa896d98
flow6852math <flow6852math@gmail.com>
2023-02-10 09:41:28 +0900

    support dup for vimgrep

--- denops/@ddu-sources/qf.ts
+++ denops/@ddu-sources/qf.ts
@@ -5,12 +5,12 @@ import {
 } from "https://deno.land/x/ddu_vim@v2.2.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.2.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.2/file.ts";
-import { equal } from "https://deno.land/x/equal@v1.5.0/mod.ts";
 
 type Params = {
   loc: boolean;
   what: What;
   isSubst: boolean;
+  dup: boolean;
   format: string;
 };
 
@@ -37,7 +37,6 @@ type What = {
   id?: number;
   lnum?: number;
   title?: string;
-  all?: number;
 };
 
 export class Source extends BaseSource<Params> {
@@ -64,61 +63,59 @@ export class Source extends BaseSource<Params> {
             ? fn.getloclist(args.denops, { id: i, all: 0 }, 0)
             : fn.getqflist(args.denops, { id: i, all: 0 })) as QuickFix;
           if (
-            // isSubst filtering title string comming soon...?
             isContain(what, args.sourceParams)
           ) {
             titleid = i;
+
+            const qflist = await (args.sourceParams.loc
+              ? fn.getloclist(args.denops, { id: titleid, all: 0 }, 0)
+              : fn.getqflist(args.denops, { id: titleid, all: 0 })) as QuickFix;
+            // create items
+            const items: Item<ActionData>[] = [];
+            // format text
+            const regexp = new RegExp(/(\s|\t|\n|\v)+/g);
+            for (const citem of qflist.items) {
+              const text: string = args.sourceParams.format.replaceAll(regexp, " ")
+                .replaceAll(
+                  "%i",
+                  String(qflist.id),
+                ).replaceAll(
+                  "%b",
+                  String(citem.bufnr),
+                ).replaceAll(
+                  "%c",
+                  String(citem.col),
+                ).replaceAll(
+                  "%l",
+                  String(citem.lnum),
+                ).replaceAll(
+                  "%T",
+                  qflist.title,
+                ).replaceAll(
+                  "%y",
+                  citem.type,
+                ).replaceAll(
+                  "%t",
+                  citem.text,
+                );
+
+              items.push({
+                word: text,
+                action: {
+                  bufNr: citem.bufnr,
+                  col: citem.col,
+                  lineNr: citem.lnum,
+                  path: await fn.bufname(args.denops, citem.bufnr),
+                  text: text,
+                },
+              });
+            }
+            controller.enqueue(items);
+            if (!args.sourceParams.dup) {
+              break;
+            }
           }
         }
-
-        if (titleid < 0) {
-          controller.close();
-        }
-
-        const qflist = await (args.sourceParams.loc
-          ? fn.getloclist(args.denops, { id: titleid, all: 0 }, 0)
-          : fn.getqflist(args.denops, { id: titleid, all: 0 })) as QuickFix;
-        // create items
-        const items: Item<ActionData>[] = [];
-        // format text
-        const regexp = new RegExp(/(\s|\t|\n|\v)+/g);
-        for (const citem of qflist.items) {
-          const text: string = args.sourceParams.format.replaceAll(regexp, " ")
-            .replaceAll(
-              "%i",
-              String(qflist.id),
-            ).replaceAll(
-              "%b",
-              String(citem.bufnr),
-            ).replaceAll(
-              "%c",
-              String(citem.col),
-            ).replaceAll(
-              "%l",
-              String(citem.lnum),
-            ).replaceAll(
-              "%T",
-              qflist.title,
-            ).replaceAll(
-              "%y",
-              citem.type,
-            ).replaceAll(
-              "%t",
-              citem.text,
-            );
-
-          items.push({
-            word: text,
-            action: {
-              bufNr: citem.bufnr,
-              col: citem.col,
-              lineNr: citem.lnum,
-              path: await fn.bufname(args.denops, citem.bufnr),
-              text: text,
-            },
-          });
-        }
-        controller.enqueue(items);
         controller.close();
       },
     });
@@ -130,6 +127,7 @@ export class Source extends BaseSource<Params> {
       what: {},
       isSubst: false,
       format: "%T|%t",
+      dup: false,
     };
   }
 }
